3f06cedd4142dee0297f0e8bc765b588769abd98
Rediet Tesfaye <rediettesfaye09@gmail.com>
2026-05-01 20:16:37 +0300

    fix: add gitignore and remove unnecessary files

--- dist/enums/LoanStatus.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LoanStatus = void 0;
-var LoanStatus;
-(function (LoanStatus) {
-    LoanStatus["ACTIVE"] = "ACTIVE";
-    LoanStatus["RETURNED"] = "RETURNED";
-    LoanStatus["OVERDUE"] = "OVERDUE";
-})(LoanStatus || (exports.LoanStatus = LoanStatus = {}));
